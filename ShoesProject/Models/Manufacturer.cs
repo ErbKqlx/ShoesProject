ccc4190878c48a3adaccaf57ff61d6608ffdfefc
dqks <you@example.com>
2026-03-10 14:53:30 +0500

    Изменение имен навигационных свойств классов модели

--- ShoesProject/Models/Manufacturer.cs
+++ ShoesProject/Models/Manufacturer.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ShoesProject;
+﻿namespace ShoesProject;
 
 public partial class Manufacturer
 {
--- ShoesProject/Models/Order.cs
+++ ShoesProject/Models/Order.cs
@@ -1,6 +1,4 @@
 ﻿using ShoesProject.Models;
-using System;
-using System.Collections.Generic;
 
 namespace ShoesProject;
 
@@ -20,11 +18,11 @@ public partial class Order
 
     public int IdStatuses { get; set; }
 
-    public virtual DeliveryPoint IdDeliveryPointNavigation { get; set; } = null!;
+    public virtual DeliveryPoint DeliveryPoint { get; set; } = null!;
 
-    public virtual Status IdStatusesNavigation { get; set; } = null!;
+    public virtual Status Status { get; set; } = null!;
 
-    public virtual User IdUserNavigation { get; set; } = null!;
+    public virtual User User { get; set; } = null!;
 
     public virtual ICollection<ProductsOrder> ProductsOrders { get; set; } = new List<ProductsOrder>();
 }
